7943f9cb9ca4bc74d9fe8d27bb654ff3cb913d91
Francisco  Muñoz <francisco.jm.aguilar1@gmail.com>
2023-06-25 23:46:02 -0400

    codigos

--- guias/guia_2/ejercicio5.py
+++ guias/guia_2/ejercicio5.py
@@ -1,42 +1,56 @@
-class Empleado:
-    def __init__(self, nombre, cargo):
-        self.nombre = nombre
-        self.cargo = cargo
-        self.subordinados = []
+class Empleados:
+    def __init__(self):
+        self.empleados = {}
 
-    def agregar_subordinado(self, subordinado):
-        self.subordinados.append(subordinado)
+    def agregar_empleado(self, nombre, cargo, jefe_directo=None):
+        empleado = {"Nombre": nombre, "Cargo": cargo, "Subordinados": []}
+        self.empleados[nombre] = empleado
+        if jefe_directo and jefe_directo in self.empleados:
+            self.empleados[jefe_directo]["Subordinados"].append(nombre)
 
-    def eliminar_subordinado(self, subordinado):
-        self.subordinados.remove(subordinado)
+    def eliminar_empleado(self, nombre):
+        empleado = self.empleados.pop(nombre, None)
+        if empleado:
+            subordinados = empleado["Subordinados"]
+            for subordinado in subordinados:
+                self.eliminar_empleado(subordinado)
 
-    def mostrar_subordinados(self):
-        for subordinado in self.subordinados:
-            print(subordinado.nombre)
+            for jefe_directo in self.empleados.values():
+                if nombre in jefe_directo["Subordinados"]:
+                    jefe_directo["Subordinados"].remove(nombre)
+                    jefe_directo["Subordinados"].extend(subordinados)
 
-    def __str__(self):
-        return f"{self.nombre}, {self.cargo}"
+    def mostrar_jerarquia(self):
+        if self.empleados:
+            self.mostrar_empleado("Director General", None, 0)
+        else:
+            print("No hay empleados en la jerarquía.")
 
-def jerarquia(empleado, nivel=0):
-    print(" " * nivel, end="")
-    print(empleado)
-    for subordinado in empleado.subordinados:
-        jerarquia(subordinado, nivel + 1)
+    def mostrar_empleado(self, nombre, jefe_directo, nivel):
+        empleado = self.empleados[nombre]
+        indentacion = " " * nivel
+        print(f"{indentacion}- Nombre: {empleado['Nombre']}, Cargo: {empleado['Cargo']}")
+        for subordinado in empleado["Subordinados"]:
+            self.mostrar_empleado(subordinado, nombre, nivel + 1)
 
-if __name__ == "__main__":
-    ceo = Empleado("Juan", "CEO")
-    d_financiero = Empleado("Pedro", "Director Financiero")
-    d_marketing = Empleado("Luis", "Director Marketing")
+    def obtener_jefe_directo(self, nombre):
+        for empleado in self.empleados.values():
+            if nombre in empleado["Subordinados"]:
+                return empleado["Nombre"]
+        return None
 
-    ceo.agregar_subordinado(d_financiero)
-    ceo.agregar_subordinado(d_marketing)
+jerarquia = Empleados()
 
-    gerente_proyecto = Empleado("Ana", "Gerente de Proyecto")
-    desarrollador_1 = Empleado("Carlos", "Desarrollador 1")
-    desarrollador_2 = Empleado("Miguel", "Desarrollador 2")
+jerarquia.agregar_empleado("Director General", "CEO")
+jerarquia.agregar_empleado("Gerente de Ventas", "Gerente", "Director General")
+jerarquia.agregar_empleado("Gerente de Marketing", "Gerente", "Director General")
+jerarquia.agregar_empleado("Supervisor de Ventas", "Supervisor", "Gerente de Ventas")
+jerarquia.agregar_empleado("Supervisor de Marketing", "Supervisor", "Gerente de Marketing")
+jerarquia.agregar_empleado("Vendedor 1", "Vendedor", "Supervisor de Ventas")
+jerarquia.agregar_empleado("Vendedor 2", "Vendedor", "Supervisor de Ventas")
+jerarquia.agregar_empleado("Asistente de Marketing", "Asistente", "Supervisor de Marketing")
 
-    d_financiero.agregar_subordinado(gerente_proyecto)
-    gerente_proyecto.agregar_subordinado(desarrollador_1)
-    gerente_proyecto.agregar_subordinado(desarrollador_2)
+jerarquia.mostrar_jerarquia()
 
-    jerarquia(ceo)
+jefe_directo = jerarquia.obtener_jefe_directo("Vendedor 1")
+print(f"Jefe directo: {jefe_directo}")
